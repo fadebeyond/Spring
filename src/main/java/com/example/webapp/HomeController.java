0337dfcf837ce4a85ed13ff01df32b971c2bbba0
Gaurang KAUSHIK <gaurang.kaushik@amadeus.com>
2024-02-13 20:27:24 +0530

    Simple Web app

---
+++ src/main/java/com/example/webapp/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.webapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("home")
+    public  String home(){
+        System.out.println("Hi");
+        return "home";
+    }
+}
